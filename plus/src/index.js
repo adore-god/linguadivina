ff1c036daa81e69f96c0f38677c6a8e9043272f2
adore-god <224534207+adore-god@users.noreply.github.com>
2026-07-27 08:31:58 +0100

    Sync: plus/src/index.js

--- plus/src/index.js
+++ plus/src/index.js
@@ -101,6 +101,8 @@ const INDEX_STYLE = `
   li { padding: 0 12px 19px 0; }
   .art-list ul li a { color: var(--light-on-dark); }
   .art-list ul li a:visited { color: var(--visited); }
+    .art-list ul li a:hover { color: var(--white); text-decoration:none;}
+    
   a, a:link { color: var(--link); font-weight:600; text-decoration: none; }
   a:visited { color: var(--visited); }
   a:hover { color: var(--bright-navy); text-decoration: underline; }
