5b016d69376bbb7a47a93c49ea36cceaa113fc15
adore-god <224534207+adore-god@users.noreply.github.com>
2026-07-31 21:38:32 +0100

    Sync: plus/src/index.js

--- plus/src/index.js
+++ plus/src/index.js
@@ -61,8 +61,8 @@ h1 {
     color: var(--light-on-dark);
     text-align: left;
     margin-bottom: 1em;
-    font-size: 1.5em;
-    line-height: 1.4;
+    font-size: 1.7em;
+    line-height: 1.5;
   }
 .center {text-align: center;padding: 1.3em 0.5em 1.3em 0.5em;}
 
