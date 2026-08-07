33152dec0fbfed220da3a4556e0aec813b30a0f7
adore-god <224534207+adore-god@users.noreply.github.com>
2026-08-07 08:33:20 +0100

    Sync top-level, scrolls, el

--- script-pager.js
+++ script-pager.js
@@ -183,7 +183,7 @@
     "the", "a", "an", "of", "in", "on", "and", "to", "for", "from", "is",
     "are", "was", "were", "be", "with", "by", "as", "at", "that", "this", "every", "named", "first", "only", "already", "assume", "through", "whose",
     "it", "its", "his", "her", "their", "they", "than", "our", "your", "my", "or", "but",
-    "not", "no", "so", "into", "about", "when", "who", "what", "how", "receives", "does", "already", "before", "identity", "divided", "runs", "assumed", "encoded", "itself", "sets", "fully", "calls", "holds", "gives", "mechanism", "different", "story", "enforce", "enforces", "same", "current", "prior", "form", "under", "over", "instrument", "began", "each", "different", "cannot", "other", "goes"
+    "not", "no", "so", "into", "about", "when", "who", "what", "how", "receives", "does", "already", "before", "after", "identity", "divided", "runs", "assumed", "encoded", "itself", "sets", "fully", "calls", "holds", "gives", "mechanism", "different", "story", "enforce", "enforces", "same", "current", "prior", "form", "under", "over", "instrument", "began", "each", "different", "cannot", "other", "goes", "removes", "that the"
   ]);
 
   // Matches sequences of capitalized words — allows the phrase to continue
